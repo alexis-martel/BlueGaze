8186a7f87d0b2e51c074839aabe1c0855c84de05
alexis <alex.is.here@icloud.com>
2023-09-28 21:50:30 -0400

    Initial Commit

---
+++ BlueGaze/BlueGazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueGazeApp.swift
+//  BlueGaze
+//
+//  Created by Alexis Andrew Martel on 2023-09-28.
+//
+
+import SwiftUI
+
+@main
+struct BlueGazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlueGaze/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BlueGaze
+//
+//  Created by Alexis Andrew Martel on 2023-09-28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
